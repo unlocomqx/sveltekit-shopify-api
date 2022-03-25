e9ae3371266ce3c08e04d3bf257953de9aac7f5b
unlocomqx <amxmodx.unloco@gmail.com>
2022-03-25 21:49:14 +0100

    Reformat code

--- src/utils/graphql_proxy.ts
+++ src/utils/graphql_proxy.ts
@@ -1,53 +1,36 @@
-import http from 'http';
+import { RequestEvent } from "@sveltejs/kit/types/private"
+import { AuthConfig } from "../auth/oauth/types"
 
-import {GraphqlClient} from '../clients/graphql';
-import {RequestReturn} from '../clients/http_client/types';
-import * as ShopifyErrors from '../error';
+import { GraphqlClient } from "../clients/graphql"
+import * as ShopifyErrors from "../error"
 
-import loadCurrentSession from './load-current-session';
+import loadCurrentSession from "./load-current-session"
 
-export default async function graphqlProxy(
-  userReq: http.IncomingMessage,
-  userRes: http.ServerResponse,
-): Promise<RequestReturn> {
-  const session = await loadCurrentSession(userReq, userRes);
+export default async function graphqlProxy (
+  config: AuthConfig,
+  event: RequestEvent,
+): Promise<Response> {
+  const session = await loadCurrentSession(config, event)
   if (!session) {
     throw new ShopifyErrors.SessionNotFound(
-      'Cannot proxy query. No session found.',
-    );
+      "Cannot proxy query. No session found.",
+    )
   } else if (!session.accessToken) {
     throw new ShopifyErrors.InvalidSession(
-      'Cannot proxy query. Session not authenticated.',
-    );
+      "Cannot proxy query. Session not authenticated.",
+    )
   }
 
-  const shopName: string = session.shop;
-  const token: string = session.accessToken;
-  let reqBodyString = '';
+  const shopName: string = session.shop
+  const token: string = session.accessToken
 
-  return new Promise((resolve, reject) => {
-    userReq.on('data', (chunk) => {
-      reqBodyString += chunk;
-    });
+  const json = await event.request.json()
 
-    userReq.on('end', async () => {
-      let reqBodyObject: {[key: string]: unknown} | undefined;
-      try {
-        reqBodyObject = JSON.parse(reqBodyString);
-      } catch (err) {
-        // we can just continue and attempt to pass the string
-      }
+  const options = {
+    data: json,
+  }
+  const client = new GraphqlClient(shopName, token)
 
-      try {
-        const options = {
-          data: reqBodyObject ? reqBodyObject : reqBodyString,
-        };
-        const client = new GraphqlClient(shopName, token);
-        const response = await client.query(options);
-        return resolve(response);
-      } catch (err) {
-        return reject(err);
-      }
-    });
-  });
+  const response = await client.query(options)
+  return new Response(JSON.stringify(response.body))
 }
