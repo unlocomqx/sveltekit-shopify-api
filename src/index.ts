004c0367c948e3809b8466420fcd3ccd5bbaa0aa
unlocomqx <amxmodx.unloco@gmail.com>
2022-03-26 12:46:34 +0100

    Fix import path

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import ShopifyAuth from "./auth/oauth/index.js"
 import ShopifySession from "./auth/session/index.js"
 import ShopifyClients from "./clients/index.js"
-import * as ShopifyErrors from "./error"
+import * as ShopifyErrors from "./error.js"
 import ShopifyUtils from "./utils/index.js"
 import ShopifyWebhooks from "./webhooks/index.js"
 
